41fd7551823aff953c13ee0c28c5a09f87481aea
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-09-08 23:36:06 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const TimeKey = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const getCurrentTime = function (currentTime) {
+  localStorage.setItem(TimeKey, JSON.stringify(currentTime.seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(TimeKey)) || 0);
+
