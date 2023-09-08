e845857db503d7058cf8863e5461e5dbd2b114df
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-09-09 00:42:35 +0300

    start 3rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textArea = document.querySelector('textarea');
+
+const formKey = "feedback-form-state";
+
+let formData = {
+  email: "",
+  message: "",
+}
+form.addEventListener("input", onFormInput);
+
+function onFormInput(event) {
+  formData[event.target.name]  = event.target.value;
+  localStorage.setItem(formKey, JSON.stringify(formData));
+};
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+}
