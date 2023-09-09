50bed394eefa720bdcd2653425c04f2e1114b51c
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-09-09 19:18:14 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,5 @@
 import throttle from 'lodash.throttle';
 
-
-
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const textArea = document.querySelector('textarea');
@@ -12,16 +10,35 @@ let formData = {
   email: "",
   message: "",
 }
+
+const storedDataForm = localStorage.getItem(formKey);
+
+if (storedDataForm && storedDataForm != "") {
+  const parsed = JSON.parse(storedDataForm)
+  formData.email = parsed.email;
+  formData.message = parsed.message;
+  input.value = parsed.email;
+  textArea.value = parsed.message
+
+}
+
 form.addEventListener("input", throttle(onFormInput, 500));
 
 function onFormInput(event) {
-  formData[event.target.name]  = event.target.value;
+  formData[event.target.name] = event.target.value;
   localStorage.setItem(formKey, JSON.stringify(formData));
+  console.log(formData);
 };
 
-form.addEventListener('submit', throttle(onFormSubmit, 500));
+form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-
+  formData.email = input.value;
+  formData.message = textArea.value;
+  console.log(formData);
+  input.value = "";
+  textArea.value = "";
+   localStorage.removeItem(formKey);
 }
+
