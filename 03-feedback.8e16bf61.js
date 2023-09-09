c95c6c47a9953e4977fdae35900fb3ef9e384839
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-09-09 07:54:33 +0000

    Deploying to gh-pages from @ iryna-popkova/goit-js-hw-08@42b6af2127d21b8dedcd2848208fcf76991a2410 🚀

--- 03-feedback.8e16bf61.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),t=(document.querySelector("input"),document.querySelector("textarea"),{email:"",message:""});e.addEventListener("input",(function(e){t[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))})),e.addEventListener("submit",(function(e){e.preventDefault()}))}();
-//# sourceMappingURL=03-feedback.8e16bf61.js.map
